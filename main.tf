9bfa4978f3cb71bcb0ffd6a18772a28712b2b68e
Vinod SR <vinodsr007@gmail.com>
2021-03-25 22:23:31 +0530

    Subnet changed

--- main.tf
+++ main.tf
@@ -23,5 +23,5 @@ resource "azurerm_virtual_network" "example" {
   name                = "terraformtestnetwork"
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.2.0/24"]
 }
